70da08cd9c156e2558f911c3a83bfb1b6eeeae2c
SandroJAM <sandrojam@hotmail.com>
2026-07-24 15:42:41 -0300

    chore: adiciona classe principal e lockfile do frontend

---
+++ backend/src/main/java/com/sandrojam/controlevendas/ControleVendasBebidasApplication.java
@@ -0,0 +1,13 @@
+package com.sandrojam.controlevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleVendasBebidasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleVendasBebidasApplication.class, args);
+    }
+
+}
